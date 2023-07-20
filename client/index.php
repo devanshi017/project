b4c697b92dbeba9cf5686e5797fe94928895208e
diyapatel19 <diyapatel.1973@gmail.com>
2023-07-20 21:07:00 +0530

    css

--- client/index.php
+++ client/index.php
@@ -6,9 +6,12 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+//
+    <div class="in">
     <div class="container">
         <h1>Welcome to Dashboard</h1>
         <a href="logout.php" class="btn btn-warning">Logout</a>
     </div>
+</div>
 </body>
 </html>
