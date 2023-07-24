fc1b3ec65979b7645d33bf7e6299f558af8683c7
sakshi88-sutariya <122281446+sakshi88-sutariya@users.noreply.github.com>
2023-07-24 09:10:27 +0530

    about

--- client/index.php
+++ client/index.php
@@ -3,16 +3,17 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-
-    <div class="in">
-    <div class="container">
-        <h1>Welcome to Dashboard</h1>
 <div>
 <?php include'header.php';?>
         <!--header-->
 </div>
-     
-        <a href="logout.php" class="btn btn-warning">Logout</a>
+
+    <!-- <div class="in">
+    <div class="container">
+        <h1>Welcome to Dashboard</h1>
+<div> -->
+    <div>
+    <a href="logout.php" class="btn btn-warning">Logout</a>
     <div class="content">
     <h1>Destination Wedding</h1>
     </div>
