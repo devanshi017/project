b4d3c976a9233f2a3686719c8a014bba02a6bd3c
sakshi88-sutariya <122281446+sakshi88-sutariya@users.noreply.github.com>
2023-07-24 08:55:11 +0530

    aboutus

--- client/aboutus.php
+++ client/aboutus.php
@@ -4,7 +4,7 @@
     <title> About Us </title>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" type="text/css" href="style.css">
+      <link rel="stylesheet" type="text/css" href="aboutus.css">
     
   </head>
   <body>
@@ -18,7 +18,7 @@
     <div class="container">
       <section class="about">
         <div class="about-image">
-          <img src="images/img1.png">
+          <img src="image/img1.png">
         </div>
         <div class="about-content">
         <h2>love, laughter, and happily-ever-afters on our special day!"</h2>
