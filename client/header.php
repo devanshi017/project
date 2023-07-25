ae7de21f21cbefbc392fc47e7ba0acd6591ff00c
devanshi017 <devanshipansuriya06@gmail.com>
2023-07-25 22:09:16 +0530

    home

--- client/header.php
+++ client/header.php
@@ -18,11 +18,12 @@
 
 <!-- header section starts  -->
 <header>  
-    <a href="#" class="logo">logo<span>.</span></a>
+    <a href="#" class="logo">Amore<span>.</span></a>
 
     <nav class="navbar">
     <a href="#">Home</a>
         <a href="#">Services</a>
+        <a href="#">cards</a>
         <a href="#">About us</a>
         <a href="#">Contact us</a>
     </nav>
--- client/index.php
+++ client/index.php
@@ -1,32 +1,27 @@
-<?php
-    include('database.php');
-?>
-<html>
-<head>
-    <link rel="stylesheet" type="text/css" href="style.css">
+<!DOCTYPE html>
+<html lang="en">
+    <headd>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scald=1.0">
+        <meta http-equiv="x-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="style.css">
+        
+       
 </head>
 <body>
 <div>
 <?php include'header.php';?>
         <!--header-->
 </div>
-
-    <!-- <div class="in">
-    <div class="container">
-        <h1>Welcome to Dashboard</h1>
-<div> -->
- 
-
-<video autoplay loop muted plays-inline class="back-video">
-        <source src= "2.mp4" type="video/mp4">
-</video>
-     
-        
- 
-    <div class="content">
-    
-            <h1>Destination Wedding</h1>
-    </div>
-
+    <div class="slider">
+    <div class="load">
+</div>
+<div class="content">
+<div class="principal">
+    <h1> Moments</h1>
+    <p>Your advanced and number one wedding planner</p>
+</div>
+</div>
+</div>
 </body>
 </html>
